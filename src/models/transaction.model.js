b4e2b92c4af5ffc98cf929e376f1a5edaa148412
ahadahmed9980 <abdul.ahad05112006@gmail.com>
2026-09-06 15:57:52 +0500

    working on transaction model

---
+++ src/models/transaction.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const transactionSchema = new mongoose.Schema(
+  {
+    fromAccount: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "account",
+      required: [true, "Transaction must be accociate with a from account"],
+      index: true,
+    },
+    toAccount: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "account",
+      required: [true, "Transaction must be accociate with a to account"],
+      index: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+        message:
+          'Status can be either "PENDING", "COMPLETED", "FAILED", "REVERSED"',
+      },
+      default: "PENDING",
+    },
+    amount: {
+      type: Number,
+      required: [true, "amount is required for transaction"],
+      min: [1, "Transaction amount cannot be negative"],
+    },
+    idempotanceKet: {
+      type: String,
+      required: [true, "key is required for transaction"],
+      unique: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+const transactionModel = mongoose.model("trasaction", transactionSchema);
+module.exports = transactionModel;
